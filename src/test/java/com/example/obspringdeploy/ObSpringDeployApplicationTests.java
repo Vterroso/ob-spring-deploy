da02f2b7cbde288e35eeda659dae03e877ea2a8d
Vterroso <v.terroso87@gmail.com>
2021-12-07 16:33:02 +0100

    Initial commit

---
+++ src/test/java/com/example/obspringdeploy/ObSpringDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obspringdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObSpringDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
